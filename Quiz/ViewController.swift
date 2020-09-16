97840560b67ff5cb9650fce48096ddae94e9ef12
kranthi pyreddy <kra330@gmail.com>
2020-09-16 10:18:48 -0400

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Kranthi Pyreddy on 9/16/20.
+//  Copyright © 2020 Bignerdranch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
